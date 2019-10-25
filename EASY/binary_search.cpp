ad7cbc61d94bee62aa3c1f39b28b509f1e3e45e4
aman212yadav <171210007@nitdelhi.ac.in>
2019-10-26 00:54:49 +0530

    binary_search

---
+++ EASY/binary_search.cpp
@@ -0,0 +1,30 @@
+//  https://www.geeksforgeeks.org/binary-search/
+
+#include<bits/stdc++.h>
+using namespace std;
+//time-complexi
+bool binarySearch(vector<int> &v,int l,int r,int num){
+   if(l>r)
+    return false;
+    int mid=(l+r)/2;
+    if(v[mid]==num)
+     return true;
+    else if(v[mid]>num){
+    return binarySearch(v,l,mid-1,num);
+    }else{
+    return binarySearch(v,mid+1,r,num);
+    }
+}
+int main(){
+int n,num;
+cin>>n>>num;
+vector<int> v(n);
+for(int i=0;i<n;i++)
+  cin>>v[i];
+
+bool res=binarySearch(v,0,n-1,num);
+  cout<<res<<endl;
+
+
+
+}
