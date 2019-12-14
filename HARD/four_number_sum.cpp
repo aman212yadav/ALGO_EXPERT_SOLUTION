e2ce78d1708e790d4c4310d70527d2d36e254aa5
aman212yadav <171210007@nitdelhi.ac.in>
2019-12-15 03:36:02 +0530

    ..

---
+++ HARD/four_number_sum.cpp
@@ -0,0 +1,52 @@
+
+//problem - given a number n size array and a number A ,find quadraple whose sum is equal to A.
+
+#include<bits/stdc++.h>
+using namespace std;
+struct node {
+    int a,b,c,d;
+};
+
+// average time complexity  - O(n^2)
+// space complexity - O(n^2)
+
+vector<node> fourNumberSum(int arr[],int n,int targetSum){
+              unordered_map< int ,vector< pair<int,int> >  > hashh;
+              vector<node > answers;
+              for(int i=0;i<n;i++){
+                 for(int j=i+1;j<n;j++){
+                     int sum=arr[i]+arr[j];
+                     if(hashh.find(targetSum-sum)!= hashh.end()){
+                            vector< pair<int,int> > v = hashh[targetSum-sum];
+                            for(auto k : v){
+                                 answers.push_back({k.first,k.second,arr[i],arr[j]});
+                            }
+                     }
+
+                 }
+                 for(int k=0;k<i;k++){
+                     hashh[arr[k]+arr[i]].push_back({arr[k],arr[i]});
+                 }
+              }
+              return answers;
+
+}
+
+int main(){
+
+int n,target;
+cin>>n>>target;
+int arr[n];
+for(int i=0;i<n;i++)
+  {
+   cin>>arr[i];
+  }
+
+vector< node > answers=fourNumberSum(arr,n,target);
+
+ for(auto k : answers){
+   cout<<k.a<<" , "<<k.b<<" , "<<k.c<<" , "<<k.d<<"\n";
+ }
+
+
+}
